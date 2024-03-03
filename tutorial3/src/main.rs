7f60d43359fb81e653edbeb46cdcdf699be27906
Raxuis <raclot.raphael@gmail.com>
2024-03-03 16:36:12 +0100

    Feat(tuples) : Discovered tuple indexes

--- tutorial3/src/main.rs
+++ tutorial3/src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     let tup: (i32, bool, char) = (1, true, 's');
     let _tup2: (i32, f64, char) = (1, 19.32, 's');
     println!("{:?}", tup);
+    println!("{}", tup.0);
+    println!("{}", tup.1);
+    println!("{}", tup.2);
 }
