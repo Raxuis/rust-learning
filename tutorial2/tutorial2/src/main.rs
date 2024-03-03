2bd58d5a24558545039a0e354a6077e6cea9d531
Raxuis <raclot.raphael@gmail.com>
2024-03-03 16:04:38 +0100

    Feat(const) : discovered const variables

--- tutorial2/tutorial2/src/main.rs
+++ tutorial2/tutorial2/src/main.rs
@@ -1,12 +1,4 @@
 fn main() {
-    let x = 4;
-    println!("x = {}", x);
-
-    {
-        let x = x - 2;
-        println!("x = {}", x);
-    }
-
-    let x = 5;
-    println!("x = {}", x);
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }
