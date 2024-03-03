532483232a64563d60aa45be295807c3e60f9113
Raxuis <raclot.raphael@gmail.com>
2024-03-03 16:39:00 +0100

    Feat(tuples) : Edited values in a tuple

--- tutorial3/src/main.rs
+++ tutorial3/src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let tup: (i32, bool, char) = (1, true, 's');
-    let _tup2: (i32, f64, char) = (1, 19.32, 's');
-    println!("{:?}", tup);
-    println!("{}", tup.0);
-    println!("{}", tup.1);
-    println!("{}", tup.2);
+    let mut tup: (i32, bool, char) = (1, true, 's');
+    println!("First tuple : {:?}", tup);
+    tup.0 = 33;
+    println!("Edited tuple : {:?}", tup);
 }
