11783d5aafd66ba2e2a1e905f49b099db37729de
Raxuis <raclot.raphael@gmail.com>
2024-03-03 16:44:35 +0100

    Feat(array) : Discovered array

--- tutorial3/src/main.rs
+++ tutorial3/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let mut tup: (i32, bool, char) = (1, true, 's');
-    println!("First tuple : {:?}", tup);
+    println!("Tuple first value : {}", tup.0);
+    println!("First tuple : {:?}\n", tup);
     tup.0 = 33;
-    println!("Edited tuple : {:?}", tup);
+    println!("Tuple first value : {}", tup.0);
+    println!("Edited tuple : {:?}\n", tup);
+
+    let arr: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("Array first value : {}", arr[0]);
+    println!("First array : {:?}", arr);
 }
