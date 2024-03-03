ee8fe7e7adad709846d94af6140c36123872670a
Raxuis <raclot.raphael@gmail.com>
2024-03-03 16:14:47 +0100

    Refactor(directories) : changed folder places

--- tutorial2/tutorial2/target/debug/deps/tutorial2-74d65d96cc5f59a6.d
+++
@@ -1,5 +0,0 @@
-/Users/raphael/Desktop/RAF/Rust/rust-learning/tutorial2/tutorial2/target/debug/deps/tutorial2-74d65d96cc5f59a6: src/main.rs
-
-/Users/raphael/Desktop/RAF/Rust/rust-learning/tutorial2/tutorial2/target/debug/deps/tutorial2-74d65d96cc5f59a6.d: src/main.rs
-
-src/main.rs:
--- tutorial2/tutorial2/target/debug/tutorial2.d
+++
@@ -1,1 +0,0 @@
-/Users/raphael/Desktop/RAF/Rust/rust-learning/tutorial2/tutorial2/target/debug/tutorial2: /Users/raphael/Desktop/RAF/Rust/rust-learning/tutorial2/tutorial2/src/main.rs
