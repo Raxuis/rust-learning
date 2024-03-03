79722379f10ebd0798e8ade5dd3cfa690fb482f3
Raxuis <raclot.raphael@gmail.com>
2024-03-03 15:53:02 +0100

    Feat(tutorial 2) : Passed to variable declaration

---
+++ tutorial2/tutorial2/target/debug/deps/tutorial2-74d65d96cc5f59a6.d
@@ -0,0 +1,5 @@
+/Users/raphael/Desktop/RAF/Rust/rust-learning/tutorial2/tutorial2/target/debug/deps/tutorial2-74d65d96cc5f59a6: src/main.rs
+
+/Users/raphael/Desktop/RAF/Rust/rust-learning/tutorial2/tutorial2/target/debug/deps/tutorial2-74d65d96cc5f59a6.d: src/main.rs
+
+src/main.rs:
---
+++ tutorial2/tutorial2/target/debug/tutorial2.d
@@ -0,0 +1,1 @@
+/Users/raphael/Desktop/RAF/Rust/rust-learning/tutorial2/tutorial2/target/debug/tutorial2: /Users/raphael/Desktop/RAF/Rust/rust-learning/tutorial2/tutorial2/src/main.rs
