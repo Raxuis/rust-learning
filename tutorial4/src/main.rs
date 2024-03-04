7543a8dd28bd612c802cd22b6f930293fcab919e
Raxuis <raclot.raphael@gmail.com>
2024-03-04 19:51:34 +0100

    Feat(tutorial4) : Made a function to repeat what the user typed

--- tutorial4/src/main.rs
+++ tutorial4/src/main.rs
@@ -1,5 +1,11 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Type something :");
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read line");
+    println!("What you typed : {}", input);
 }
