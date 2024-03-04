67baa7be028530acd259fa97998bca7e1e1cc9cc
Raxuis <raclot.raphael@gmail.com>
2024-03-04 20:01:43 +0100

    Test(tutorial5) : Just tested some additions with different types

--- tutorial5/src/main.rs
+++ tutorial5/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     let x: u8 = 9; // 0 <-> 255
     let y: i8 = 10; // -128 <-> 127
+
+    let z = x + y;
+    println!("{}", z); // cannot do this because no implementation for `u8 + i8`
 }
