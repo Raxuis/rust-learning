aba0cc44041e60e3c8e98ea41ee30e379691f20f
Raxuis <raclot.raphael@gmail.com>
2024-03-04 20:04:18 +0100

    Test(tutorial5) : Just tested some additions with different types

--- tutorial5/src/main.rs
+++ tutorial5/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let x: i64 = 9; // 0 <-> 255
-    let y: i8 = 10; // -128 <-> 127
+    let x: u8 = 255; // 0 <-> 255
+    let y: u8 = 1; // 0 <-> 255
 
     let z = x + y;
-    println!("{}", z); // cannot do this because no implementation for `i64 + i8`
+    println!("{}", z); // cannot do this because attempt to compute `u8::MAX + 1_u8`, which would overflow
 }
