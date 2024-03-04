148be4ef85d9bb684619d3a03fc25a3852807251
Raxuis <raclot.raphael@gmail.com>
2024-03-04 20:07:41 +0100

    Test(tutorial5) : Tried to divide 255 by 10 but both variables are u8

--- tutorial5/src/main.rs
+++ tutorial5/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let x: u8 = 255; // 0 <-> 255
-    let y: u8 = 1; // 0 <-> 255
+    let y: u8 = 10; // 0 <-> 255
 
-    let z = x + y;
-    println!("{}", z); // cannot do this because attempt to compute `u8::MAX + 1_u8`, which would overflow
+    let z = x / y;
+    println!("{}", z); // Gives 25 but the result is 25.5
 }
