9d1e06fee08c76d11f7aa23e65883101b7cb12c3
Raxuis <raclot.raphael@gmail.com>
2024-03-04 20:36:16 +0100

    Test(tutorial5) : Tried something but can't handle string types

--- tutorial5/src/main.rs
+++ tutorial5/src/main.rs
@@ -1,7 +1,25 @@
-fn main() {
-    let x = 25 as i8;
-    let y = 10 as i32;
+use std::io;
 
-    let z = x / y as i8;
-    println!("{}", z);
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error reading the line");
+    let value_response = value_check(input.trim().parse().unwrap());
+    if value_response {
+        println!("Your input is an integer");
+    } else {
+        println!("Your input is not an integer");
+    }
+}
+fn value_check(value: i64) -> bool {
+    if value < 0 {
+        println!("{} is negative", value);
+    } else if value > 0 {
+        println!("{} is positive", value);
+    } else {
+        println!("{} is zero", value);
+        return false;
+    }
+    return true;
 }
