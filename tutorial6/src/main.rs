18478a9a87cf0c3da74dc2209f1364026ff9dafb
Raxuis <raclot.raphael@gmail.com>
2024-03-09 12:31:08 +0100

    Test(tutorial6) : Testing if condition with else if

--- tutorial6/src/main.rs
+++ tutorial6/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let food = "cookie";
     if food == "cookie" {
         println!("Miam a delicious {}.", food);
+    } else if food == "fruit" {
+        println!("A {} is not thaht bad for my health!", food)
     } else {
         println!("Why would I eat {} when I can eat cookies.", food);
     }
