d301e1122f533ca9720b43deaacdf5f326a46f00
Raxuis <raclot.raphael@gmail.com>
2024-03-09 12:29:24 +0100

    Test(tutorial6) : Testing if condition

--- tutorial6/src/main.rs
+++ tutorial6/src/main.rs
@@ -1,14 +1,8 @@
 fn main() {
-    // Compound conditions
-    // && : and
-    // || : or
-    // ! : not
-    let cond = (2 as f32) < 3.4;
-    let cond2 = false && cond;
-    let cond3 = false || cond;
-    let cond4 = !cond;
-    println!("{}", cond);
-    println!("{}", cond2);
-    println!("{}", cond3);
-    println!("{}", cond4);
+    let food = "cookie";
+    if food == "cookie" {
+        println!("Miam a delicious {}.", food);
+    } else {
+        println!("Why would I eat {} when I can eat cookies.", food);
+    }
 }
