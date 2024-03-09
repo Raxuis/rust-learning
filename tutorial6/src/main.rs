7965ce1f606cc5e0b5a9e5c84a9776888ead45b2
Raxuis <raclot.raphael@gmail.com>
2024-03-09 12:21:39 +0100

    Test(tutorial6) : Testing compound conditions

--- tutorial6/src/main.rs
+++ tutorial6/src/main.rs
@@ -5,6 +5,9 @@ fn main() {
     // ! : not
     let cond = (2 as f32) < 3.4;
     let cond2 = false && cond;
-    println!("{}", cond);
+    let cond3 = false || cond;
+    let cond4 = !false;
     println!("{}", cond2);
+    println!("{}", cond3);
+    println!("{}", cond4);
 }
