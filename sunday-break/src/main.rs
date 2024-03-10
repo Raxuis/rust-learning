66527f16475ec3d01107aecb07af63e1e633d699
Raxuis <raclot.raphael@gmail.com>
2024-03-10 18:02:15 +0100

    Test(sunday-break) : Decided to use object-orientation

--- sunday-break/src/main.rs
+++ sunday-break/src/main.rs
@@ -1,15 +1,1 @@
-use std::io;
-
-fn main() {
-    println!("What's the height of your table?");
-    let mut height = String::new();
-    io::stdin().read_line(&mut height).expect("Can't read it!");
-    println!("What's the width of your table?");
-    let mut width = String::new();
-    io::stdin().read_line(&mut width).expect("Can't read it!");
-    println!(
-        "Your table's dimensions : {}x{}",
-        height.trim(),
-        width.trim()
-    );
-}
+trait Table {}
